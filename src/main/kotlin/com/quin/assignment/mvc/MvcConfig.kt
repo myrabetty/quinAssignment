395c1dd116324433de9fe9c8993a3b8c31dd4610
elisabetta.semboloni <elisabetta.semboloni@bestseller.com>
2019-10-18 16:06:49 +0200

    Moving on. Now I have an home page!

---
+++ src/main/kotlin/com/quin/assignment/mvc/MvcConfig.kt
@@ -0,0 +1,26 @@
+package com.quin.assignment.mvc;
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+/** Model view controller config.
+ **/
+
+@Configuration
+class MvcConfig : WebMvcConfigurer {
+
+    /**
+     * login url.
+     */
+    val home = "/home"
+
+    /**
+     * login model view.
+     */
+    val homeView = "home"
+
+    override fun addViewControllers(registry: ViewControllerRegistry) {
+        registry.addViewController(home).setViewName(home)
+    }
+}
